909a6c38b2a568bcca03c84a69316fb3dda8e55d
cristhofert <cristhofert97@gmail.com>
2021-04-27 20:11:21 +0000

    Add Card Component

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Card } from "./card";
 
 //create your first component
 export function Home() {
@@ -11,6 +12,13 @@ export function Home() {
 			<p>
 				<img src={rigoImage} />
 			</p>
+			<Card
+				imageUrl="https://picsum.photos/500/325"
+				title="Lorem"
+				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus sem, auctor vel ante in, dictum tristique arcu. Praesent ornare nibh id congue vulputate. Nulla ultricies in ipsum a tristique. Maecenas accumsan in felis ut ultrices. Proin scelerisque consequat"
+				buttonLabel="Go To"
+				buttonUrl="https://es.wikipedia.org"
+			/>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working
 			</a>
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { Card } from "./component/card.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
